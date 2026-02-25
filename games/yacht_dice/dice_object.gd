b4713c6f6cabc9e7b301df2f6e81163be7824c5f
ZombieNW <59242209+ZombieNW@users.noreply.github.com>
2026-02-25 13:53:41 -0700

    Continue game logic

---
+++ games/yacht_dice/dice_object.gd
@@ -0,0 +1,29 @@
+extends Control
+class_name DiceObject
+
+@export var value: int = 1
+@export var held: bool = false
+
+@onready var dice_textures: Dictionary[int, Texture] = {
+	1: preload("uid://b7rgvjc6e1gi3"),
+	2: preload("uid://bpqjqkcalfo0a"),
+	3: preload("uid://fvqqgslls8np"),
+	4: preload("uid://c4g3vgjoa2d5n"),
+	5: preload("uid://cpfi6oqi872ap"),
+	6: preload("uid://cg2hbbto2adc2")
+}
+
+func roll() -> void:
+	value = randi_range(1, 6)
+	update_state()
+
+func set_held(is_held: bool) -> bool:
+	held = is_held
+	$HeldLabel.visible = held
+	return held
+
+func toggle_held() -> void:
+	held = set_held(!held)
+
+func update_state() -> void:
+	$TextureRect.texture = dice_textures[value]
--- games/yacht_dice/game_manager.gd
+++ games/yacht_dice/game_manager.gd
@@ -1,28 +1,30 @@
 extends Node2D
 
+# Game State
 enum GameState { BET, INITIAL_DICE, FINAL_DICE, SCORE, FINAL_SCORE}
-
 var game_state: GameState = GameState.BET
 
+# Betting
+const POINT_PAYOUT_RATIO: int = 100
 const MAX_BET: int = 5
 var bet: int = 1
 
-@onready var dice_objects: Array[TextureRect] = [%Dice1, %Dice2, %Dice3, %Dice4, %Dice5]
+# Gameplay
+@onready var dice_objects: Array[DiceObject] = [%Dice1, %Dice2, %Dice3, %Dice4, %Dice5]
 var dice_values: Array[int] = [1, 1, 1, 1, 1]
-@onready var hold_labels: Array[Label] = [%Hold1, %Hold2, %Hold3, %Hold4, %Hold5]
-@onready var dice_textures: Dictionary[int, Texture] = {
-	1: preload("uid://b7rgvjc6e1gi3"),
-	2: preload("uid://bpqjqkcalfo0a"),
-	3: preload("uid://fvqqgslls8np"),
-	4: preload("uid://c4g3vgjoa2d5n"),
-	5: preload("uid://cpfi6oqi872ap"),
-	6: preload("uid://cg2hbbto2adc2")
-}
+var held: Array[int] = []
 
 func _ready() -> void:
 	update_state()
 
 func _input(event: InputEvent) -> void:
+	# Hold Buttons
+	for i in range(dice_objects.size()):
+		if event.is_action_pressed("hold_%d" % (i + 1)):
+			hold_die(i)
+			return
+	
+	# Action Buttons
 	if event.is_action_pressed("select_game"):
 		exit_game()
 	elif event.is_action_pressed("bet"):
@@ -31,7 +33,51 @@ func _input(event: InputEvent) -> void:
 		await get_tree().process_frame
 		update_state()
 	elif event.is_action_pressed("deal_draw"):
-		roll_dice()
+		change_game_state()
+
+# Hold given dice
+func hold_die(index: int) -> void:
+	if game_state != GameState.INITIAL_DICE: return
+	
+	if index in held:
+		held.erase(index)
+	else:
+		held.append(index)
+	held.sort()
+	update_state()
+
+# Advance game
+func change_game_state() -> void:
+	match game_state:
+		GameState.BET:
+			start_game()
+		GameState.INITIAL_DICE:
+			middle_turn()
+		GameState.FINAL_DICE:
+			pass
+			#evaluate()
+
+# Accept bet and roll first dice
+func start_game() -> void:
+	if Credit.get_credits() < bet:
+		%StateLabel.text = "Insufficient Credits"
+		return
+	
+	%StateLabel.text = ""
+	Credit.subtract(bet)
+	game_state = GameState.INITIAL_DICE
+	roll_dice()
+	update_state()
+
+# Middle Turn
+func middle_turn() -> void:
+	game_state = GameState.FINAL_DICE
+	roll_dice()
+	evaluate()
+	update_state()
+
+func evaluate() -> void:
+	%StateLabel.text = "Hand Go Here"
 
 func exit_game() -> void:
 	if game_state == GameState.BET or game_state == GameState.FINAL_SCORE:
@@ -45,11 +91,14 @@ func cycle_bet() -> void:
 func update_state() -> void:
 	%CreditLabel.text = "%d Credits" % Credit.get_credits()
 	%BetLabel.text = "Bet %d" % bet
+	%PayoutLabel.text = "%d Credits / %d Points" % [bet, POINT_PAYOUT_RATIO]
+	
 	for dice_idx in dice_objects.size():
-		dice_objects[dice_idx].texture = dice_textures.get(dice_values[dice_idx])
+		dice_objects[dice_idx].set_held(dice_idx in held)
 
+# Rolls all dice
 func roll_dice() -> void:
 	for dice_idx in dice_objects.size():
-		var rand: int = randi_range(1, 6)
-		dice_values[dice_idx] = rand
+		if dice_idx in held: continue
+		dice_objects[dice_idx].roll()
 	update_state()
